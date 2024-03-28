17779f97c7e32f557cd42b7110db6fda66a6a893
Umut Guler <bluethdoctor@gmail.com>
2024-03-29 01:19:16 +0300

    Added Chane Password

--- js/updateUser.js
+++ js/updateUser.js
@@ -34,5 +34,45 @@ document.addEventListener("DOMContentLoaded", function() {
         console.error("Hata:", error.message);
       });
     });
+
+// Change Password
+
+    const updatePassword = document.getElementById("update-password");
+  
+    updatePassword.addEventListener("submit", function(event) {
+      event.preventDefault();
+  
+      const formData = new FormData(updatePassword);
+      const requestData = {};
+      formData.forEach((value, key) => {
+        requestData[key] = value;
+      });
+  
+      const apiUrl = "https://localhost:7190/api/userInfo/change-password";
+      const token = localStorage.getItem("accessToken");
+  
+      fetch(apiUrl, {
+        method: "PUT",
+        headers: {
+          "Authorization": `Bearer ${token}`,
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(requestData)
+      })
+      .then(response => {
+        if (response.ok) {
+          alert("Şifre başarıyla güncellendi.");
+          localStorage.removeItem('accessToken');
+          window.location.href = 'login.html';
+        } else {
+          throw new Error("Şifre güncellenirken bir hata oluştu.");
+        }
+      })
+      .catch(error => {
+        console.error("Hata:", error.message);
+      });
+    });
+
   });
   
